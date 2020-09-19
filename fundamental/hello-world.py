1e9718070025b9d2870f27a9502200d7938f5353
Walid nurudin <walidnurudin47@gmail.com>
2020-09-19 22:29:34 +0700

    first

---
+++ fundamental/hello-world.py
@@ -0,0 +1,12 @@
+# VARIABEL
+nama = 'walid nurudin'
+_kelas = 'frontend'
+print(nama, _kelas)
+
+print('Walid\nNurudin')
+print('Walid\tNurudin')
+print('\'Walid\'')
+
+# METHODS STRING
+print(nama.capitalize()) # Walid nurudin
+print(nama.replace('i', 'e')) # waled nuruden
