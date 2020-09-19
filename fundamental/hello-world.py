bafad5599ac5216b0bc6cfd83cba02119857d5a5
Walid nurudin <walidnurudin47@gmail.com>
2020-09-19 23:14:15 +0700

    number

--- fundamental/hello-world.py
+++ fundamental/hello-world.py
@@ -1,7 +1,12 @@
+"""
+Belajar bahasa python
+(ini komentar)
+"""
+
 # VARIABEL
-nama = 'walid nurudin'
-_kelas = 'frontend'
-print(nama, _kelas)
+nama = 'walid nurudin '
+_kelas = input("masuk kelas apa? ")
+print(nama + _kelas)
 
 # string
 print('Walid\nNurudin')
@@ -15,7 +20,9 @@ print(nama.replace('i', 'e')) # waled nuruden
 # number
 x = 10
 y = 20
-print(x * y) 
+print(x * y)
+
+# methods number
 print(max(x, y)) # 20
 print(min(x, y)) # 10
 print(round(8.14)) # 8
@@ -23,3 +30,10 @@ print(round(8.70)) # 9
 
 import math
 print(math.pi) # 3.14
+
+# string & number
+print(nama, x) # walid nurudin 10
+print(nama + str(x)) # walid nurudin10
+
+s = '5'
+print(x + int(s)) # 15
