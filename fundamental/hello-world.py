ac896aa7394965f9fc644274b4177acb70b77689
Walid nurudin <walidnurudin47@gmail.com>
2020-09-19 22:50:02 +0700

    math

--- fundamental/hello-world.py
+++ fundamental/hello-world.py
@@ -3,10 +3,23 @@ nama = 'walid nurudin'
 _kelas = 'frontend'
 print(nama, _kelas)
 
+# string
 print('Walid\nNurudin')
 print('Walid\tNurudin')
 print('\'Walid\'')
 
-# METHODS STRING
+# methods string
 print(nama.capitalize()) # Walid nurudin
 print(nama.replace('i', 'e')) # waled nuruden
+
+# number
+x = 10
+y = 20
+print(x * y) 
+print(max(x, y)) # 20
+print(min(x, y)) # 10
+print(round(8.14)) # 8
+print(round(8.70)) # 9
+
+import math
+print(math.pi) # 3.14
